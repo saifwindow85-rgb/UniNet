f41ba94ef74dfbe6c5ac692e4c25eefe07031d15
saifwindow85-rgb <saifwindow85@gmail.com>
2026-07-04 16:02:23 +0300

    Add ICurrentUserService Add CurrentUserService

---
+++ Application/Services/IdentityServices/CurrentUserService.cs
@@ -0,0 +1,38 @@
+﻿using Domain.Interfaces.UserInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+namespace Application.Services.IdentityServices
+{
+    public class CurrentUserService : ICurrentUserService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        
+        public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public int UserId { get 
+            { var userId = _httpContextAccessor.HttpContext?.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+                if (userId == null)
+                    throw new UnauthorizedAccessException("User is not authenticated.");
+                return int.Parse(userId);
+            } }
+
+        public string UserName
+        {
+            get
+            {
+                var userName = _httpContextAccessor.HttpContext?.User.FindFirst(ClaimTypes.Name)?.Value;
+                if (userName == null)
+                    throw new UnauthorizedAccessException("User is not authenticated.");
+                return userName;
+            }
+        }
+    }
+}
---
+++ Domain/Interfaces/UserInterfaces/ICurrentUserService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.UserInterfaces
+{
+    public interface ICurrentUserService
+    {
+        public int UserId { get; }
+        public string UserName { get; }
+    }
+}
