d093492d96d2919efb2d4fa28be9464e6c427d06
saifwindow85-rgb <saifwindow85@gmail.com>
2026-08-03 10:38:50 +0300

    -Update UpdateStudentDTO(Remove BatchId)

--- Contracts/Requests/StudentRequests/UpdateStudentDTO.cs
+++ Contracts/Requests/StudentRequests/UpdateStudentDTO.cs
@@ -9,7 +9,6 @@ namespace Contracts.Requests.StudentRequests
 {
     public class UpdateStudentDTO : BaseUpdateEmployeeOrStudentDTO
     {
-        public int BatchId { get; set; }
         public int? SectionId { get; set; }
         public int StatusId { get; set; }
     }
